e8207baba77607a13606a3e5bda6ca63fc164c38
Dragana Kucanski <draganakucanski@gmail.com>
2022-06-18 12:45:03 +0200

    Registration page partly done

---
+++ Web-Project/static/js/redirect.js
@@ -0,0 +1,11 @@
+function redirectOnLogin() {
+	window.location.href = "/login.html";
+}
+
+function redirectOnRegistration() {
+	window.location.href = "/registration.html";
+}
+
+function loadFirstPage() {
+	window.location.href = "/homePage.html";
+}
